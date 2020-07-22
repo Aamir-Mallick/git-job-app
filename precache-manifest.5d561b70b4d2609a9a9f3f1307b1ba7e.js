04190a32c2d83086423913d1ad338e1e72c6fe33
Md Aamir Mallick <aamirmallick71@gmail.com>
2020-07-22 10:51:52 +0530

    Updates

---
+++ precache-manifest.5d561b70b4d2609a9a9f3f1307b1ba7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d10a72ffbb79f20ce5e5295fd66c0d7",
+    "url": "/git-job-app%20/index.html"
+  },
+  {
+    "revision": "e4a6e8fe2e7c5ab76d6d",
+    "url": "/git-job-app%20/static/js/2.12e6e921.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/git-job-app%20/static/js/2.12e6e921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a787340790a1a3e74325",
+    "url": "/git-job-app%20/static/js/main.684851ba.chunk.js"
+  },
+  {
+    "revision": "fe20cc12596dedea2e7d",
+    "url": "/git-job-app%20/static/js/runtime-main.eba92e4d.js"
+  }
+]);
